7d8cbc9bd85c95294dcb596f29278803da802e6a
aliira16 <rouaneali27@gmail.com>
2026-04-17 10:46:24 +0100

    create project folder and run based server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from "express"
+import cors from "cors"
+import pg from "pg"
+
+
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("hello there");
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+})
